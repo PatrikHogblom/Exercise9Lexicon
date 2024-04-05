780d6ce917cee80e08cf9b20c4051b5af5640a3f
PatrikHogblom <patrik.hogblom@live.se>
2024-04-05 15:31:20 +0200

    have mostly made a shoplist that is required for the task.  Will make some style changes later or if time permits then add more functionality

--- ShoppingList/shoppingList.js
+++ ShoppingList/shoppingList.js
@@ -14,8 +14,51 @@ function addItem()
 
     //create a new list item
     let newListItem = document.createElement("li");
-    newListItem.className = "list-group-item text-start";
-    newListItem.textContent = newItemInput;
+    newListItem.className = "list-group-item";
+
+    //create a span for the text 
+    let textspan = document.createElement("span");
+    textspan.className = "float-start";
+    textspan.textContent = newItemInput;
+
+    //add bought button to the list
+    let boughtButton = document.createElement("button");
+    boughtButton.className = "btn btn-success btn-sm float-end";
+    boughtButton.textContent = "Bought";
+
+    boughtButton.onclick = function(){
+        newListItem.classList.add("list-group-item-success");
+        textspan.classList.add("text-decoration-line-through");
+        boughtButton.remove();
+
+        //add undo button
+        //add the undo button
+        let undoButton = document.createElement("button");
+        undoButton.className = "btn btn-warning btn-sm float-end";
+        undoButton.textContent = "Undo";
+
+        undoButton.onclick = function(){
+            newListItem.classList.remove("list-group-item-success");
+            textspan.classList.remove("text-decoration-line-through");
+            undoButton.remove();
+            newListItem.appendChild(boughtButton);
+        }
+        newListItem.appendChild(undoButton);
+    }
+    
+    //add delete button to the list
+    let deleteButton = document.createElement("button");
+    deleteButton.className = "btn btn-danger btn-sm float-end";
+    deleteButton.textContent = "X";
+
+    deleteButton.onclick = function(){
+        this.parentNode.remove();
+    }
+    
+    newListItem.appendChild(textspan);
+    newListItem.appendChild(deleteButton);
+    newListItem.appendChild(boughtButton);
+
 
     //append the new item to the list 
     document.getElementById("itemContainerList").appendChild(newListItem);
